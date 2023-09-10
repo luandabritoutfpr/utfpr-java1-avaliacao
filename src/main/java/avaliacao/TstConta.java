62444b5b71c1ba7f8d9063062ae886747cf32868
Luanda Brito <49211714+luandabrito@users.noreply.github.com>
2023-09-10 11:16:57 -0300

    add testes para cenários não felizes

--- src/main/java/avaliacao/TstConta.java
+++ src/main/java/avaliacao/TstConta.java
@@ -8,13 +8,15 @@ public class TstConta {
 
         try {
 
-            //teste de cenário feliz!
-            pj.setNumeroConta(3);
+            //entrada de dados
+            pj.setNumeroConta(4);
             pj.setCnpj(1234);
             pj.getEnder().setRua("travessia central");
             pj.getResponsavel().setCpf(15);
             pj.getResponsavel().setNome("Maria");
 
+            //saída de dados
+            System.out.println("\nCenário Feliz! Número conta positivo e par, CPF válido e Nome válido.");
             System.out.println("Número da conta: " + pj.getNumeroConta());
             pj.validar();
             System.out.println("CNPJ: " + pj.getCnpj());
@@ -23,6 +25,28 @@ public class TstConta {
             pj.getResponsavel().verifDoc();
             System.out.println("Nome do responsável: " + pj.getResponsavel().getNome());
             pj.verifDoc();
+
+            //entrada de dados
+            pj.setNumeroConta(3);
+            pj.setCnpj(1234);
+            pj.getEnder().setRua("travessia central");
+            pj.getResponsavel().setCpf(123);
+            pj.getResponsavel().setNome("Maria da silva santos oliveira souza de lima dantas");
+
+            //saída de dados
+            System.out.println("\nCenário não feliz! Número conta positivo e impar, CPF inválido e Nome inválido.");
+            System.out.println("Número da conta: " + pj.getNumeroConta());
+            pj.validar();
+            System.out.println("CNPJ: " + pj.getCnpj());
+            System.out.println("Rua: " + pj.getEnder().getRua());
+            System.out.println("CPF do responsável: " + pj.getResponsavel().getCpf());
+            pj.getResponsavel().verifDoc();
+            System.out.println("Nome do responsável: " + pj.getResponsavel().getNome());
+            pj.verifDoc();
+
+            //entrada de dados
+            System.out.println("\nCenário com exceção! Número conta negativo!");
+            pj.setNumeroConta(-10);
 
         } catch (NumException e) {
             e.impMsg();
